16fa79a05346c43afee6b6097dbb19d1fd2f2e86
icxd <icoding666@gmail.com>
2023-06-08 20:46:08 +0200

    added AST_TOP_LEVEL_ATTRIBUTE case to ast_print function

--- src/ast.c
+++ src/ast.c
@@ -30,13 +30,25 @@ void ast_print(struct ast_t* ast, u32 indent) {
             break;
         }
         case AST_BINARY: {
+            printf("AST_BINARY: "SV_ARG"\n", SV_FMT(ast->data.binary_op.op.value));
+            ast_print(ast->data.binary_op.left, indent + 1);
+            ast_print(ast->data.binary_op.right, indent + 1);
             break;
         }
         case AST_UNARY: {
+            printf("AST_UNARY: "SV_ARG"\n", SV_FMT(ast->data.unary_op.op.value));
+            ast_print(ast->data.unary_op.left, indent + 1);
             break;
         }
         case AST_VARIABLE: {
             printf("AST_VARIABLE: \""SV_ARG"\"\n", SV_FMT(ast->data.variable.identifier));
+            break;
+        }
+        case AST_TOP_LEVEL_ATTRIBUTE: {
+            printf("AST_TOP_LEVEL_ATTRIBUTE: \""SV_ARG"\"\n", SV_FMT(ast->data.top_level_attribute.name));
+            if (ast->data.top_level_attribute.value != NULL) {
+                ast_print(ast->data.top_level_attribute.value, indent + 1);
+            }
             break;
         }
         default: {
